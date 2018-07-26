0a52a488c0b63fdf5f137f4ae12777373096c6fe
Kristoffer <kba_93@live.se>
2018-07-26 15:17:10 +0900

    Test fix

--- ir/led.py
+++ ir/led.py
@@ -12,18 +12,18 @@ GPIO.setup(37, GPIO.OUT)
 # 37left
 def toggle_led(runKey):
     if runKey == 'right':
-        GPIO.output(33, True)
-        GPIO.output(35, False)
-        GPIO.output(37, False)
-    elif runKey == 'center':
         GPIO.output(33, False)
         GPIO.output(35, True)
-        GPIO.output(37, False)
-    elif runKey == 'left':
-        GPIO.output(33, False)
+        GPIO.output(37, True)
+    elif runKey == 'center':
+        GPIO.output(33, True)
         GPIO.output(35, False)
         GPIO.output(37, True)
-    else:
-        GPIO.output(33, False)
-        GPIO.output(35, False)
+    elif runKey == 'left':
+        GPIO.output(33, True)
+        GPIO.output(35, True)
         GPIO.output(37, False)
+    else:
+        GPIO.output(33, True)
+        GPIO.output(35, True)
+        GPIO.output(37, True)
