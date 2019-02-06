299d09e4f160f4855b6c09c8535cf9d2891af956
tama <tamatama@tama-no-Air.int.kcg.edu>
2019-02-06 10:12:22 +0900

    first commit

--- receiver.py
+++ receiver.py
@@ -2,7 +2,7 @@ import time
 import redis
 import json
 
-import IRControlSystem.transmitter as ir
+import IRControlSystem.lib.Ir_sender.transmitter as ir
 
 
 class Receiver:
@@ -12,8 +12,8 @@ class Receiver:
     runCodeQueue = redis.Redis(host='localhost', port=6379)
     mode = ["c_", "h_", "d_"]
     strength = ["dir3_str1.json", "dir3_str3.json", "dir3_str4.json"]
-    # 緊急コード
-    DIR_NAME = "IRControlSystem/Ir_code/"
+    DIR_NAME = "IRControlSystem/lib/Ir_code/"
+    DRY_MAP = {}
 
     @classmethod
     def run(self):
@@ -26,7 +26,7 @@ class Receiver:
 
                     if order_list["on"] == "on":
                         if order_list["mode"] == 2:
-                            runKey = "2_0"
+                            runKey = str(order_list["temperature"])
                             fileName = self.DIR_NAME + "d_dir3.json"
 
                         else:
@@ -35,15 +35,15 @@ class Receiver:
                             runKey = str(order_list["temperature"])
                             fileName = self.DIR_NAME + keyM + keyS
 
-                        ir.boot("on")
+                        ir.boot("on", self.DIR_NAME)
                         time.sleep(self.SURVEILLANCE_INTERVAL * 3)
                         ir.transmission(runKey, fileName)
                         time.sleep(self.SURVEILLANCE_INTERVAL * 3)
                     elif order_list["on"] == "off":  # 電源オフ
-                        ir.boot("off")
+                        ir.boot("off", self.DIR_NAME)
                         time.sleep(self.SURVEILLANCE_INTERVAL * 3)
                         # 二回停止しないと洗浄モードが切れないため
-                        ir.boot("off")
+                        ir.boot("off", self.DIR_NAME)
                         time.sleep(self.SURVEILLANCE_INTERVAL * 3)
                     else:
                         print("error order code")
